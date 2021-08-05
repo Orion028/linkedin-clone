265282dd5d635f4a9345cbb2cf8b870a077c3774
Orion028 <sumansasmal028@gmail.com>
2021-08-05 23:38:24 +0530

    header done

--- src/Header.js
+++ src/Header.js
@@ -4,20 +4,27 @@ import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
 function Header() {
     return (
         <div className='header'>
             <div className="header_left">
                 <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt="LinkedIn" />
-            </div>
-            <div className="header_search">
-                <SearchIcon />
-                <input type="text" />
-
+                <div className="header_search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
             </div>
             <div className="header_right">
                 <HeaderOption Icon={HomeIcon} title="Home" />
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Job" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notification" />
+                <HeaderOption avatar={"https://lh3.googleusercontent.com/a-/AOh14GiuV2sONhrLHhWakNguWFooruFGeFhkf7EK8vUG7A=s288-p-rw-no"} title="Me" />
 
             </div>
 
---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react'
+import "./sidebar.css";
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            <div className="side__tap">
+                <img src="" alt="" />
+                <Avatar />
+                <h2>Suman Sasmal</h2>
+                <h4>sumansasmal028@gmail.com</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
