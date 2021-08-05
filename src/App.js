7124202cf7941ac7069360113a5b9b772611da78
Orion028 <sumansasmal028@gmail.com>
2021-08-06 01:21:24 +0530

    sidebartop done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-            <Header />
-            <Sidebar />
+      <Header />
+        <div className="app__body">
+          <Sidebar />  
+        </div>
     </div>
   );
     
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,14 +2,44 @@ import { Avatar } from '@material-ui/core';
 import React from 'react'
 import "./sidebar.css";
 function Sidebar() {
+    
+    const recentItem = (topic) => {
+    <div className="sidebar__recentItem">
+        <span className="sidebar__hash">#</span>
+        <p>{topic}</p>
+    </div>         
+    };
+    
     return (
         <div className="Sidebar">
-            <div className="side__tap">
-                <img src="" alt="" />
-                <Avatar />
+            <div className="sidebar__top">
+                <img src="https://images.unsplash.com/photo-1554034483-04fda0d3507b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM3fHxiYWNrZ3JvdW5kfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Cover img" />
+                <Avatar className="sidebar__avatar"/>
                 <h2>Suman Sasmal</h2>
                 <h4>sumansasmal028@gmail.com</h4>
             </div>
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <div className="sidebar__statnumber">
+                        2,456
+                    </div>
+                </div>
+                <div className="sidebar__stat">
+                <p>Who viewed you</p>
+                    <div className="sidebar__statnumber">
+                        2,386
+                    </div>
+                </div>
+            </div>
+                <div className="sidebar__bottom">
+                    <p>Recent</p>
+                    {recentItem('Reactjs')}
+                    {recentItem("linkedin")}
+                    {recentItem("programming")}
+                    {recentItem("design")}
+                    {recentItem("developer")}
+                </div>
         </div>
     )
 }
